1d30a1f5a07026c6ffc9a92b49e8d22465195a67
muhilvarsini <muhilvarsini@gmail.com>
2021-02-04 20:14:09 +0530

    user setup

--- app/src/main/java/com/example/whee4/User.java
+++ app/src/main/java/com/example/whee4/User.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -22,6 +23,40 @@ public class User extends Fragment implements View.OnClickListener{
         view = inflater.inflate(R.layout.fragment_user, container, false);
         logoutbtn = view.findViewById(R.id.logoutbutton);
         logoutbtn.setOnClickListener(this);
+
+        LinearLayout l = (LinearLayout) view.findViewById(R.id.linearlayout_1);
+        l.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent activity_found = new Intent(getActivity(), FoundActivity.class);
+                startActivity(activity_found);
+            }
+
+        });
+        LinearLayout l1 = (LinearLayout) view.findViewById(R.id.linearlayout_2);
+        l1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent activity_lost = new Intent(getActivity(), ProfileLost.class);
+                startActivity(activity_lost);
+            }
+        });
+        LinearLayout l2 = (LinearLayout) view.findViewById(R.id.linearlayout_4);
+        l2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent activity_settings = new Intent(getActivity(), ProfileSettings.class);
+                startActivity(activity_settings);
+            }
+        });
+        LinearLayout l3 = (LinearLayout) view.findViewById(R.id.linearlayout_5);
+        l3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent activity_logout = new Intent(getActivity(), ProfileLogout.class);
+                startActivity(activity_logout);
+            }
+        });
         return view;
     }
     @Override
